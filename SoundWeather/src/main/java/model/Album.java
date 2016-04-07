a896f32f5ff1f8dc29e53702f23f53d915a801ee
radgospodinov Radoslav Gospodinov <rad.gospodinov@gmail.com>
2016-04-07 23:52:23 +0300

    cover of the album is now string fileName instead of byte[]

--- SoundWeather/src/main/java/model/Album.java
+++ SoundWeather/src/main/java/model/Album.java
@@ -14,68 +14,83 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "albums")
+@Table(name = "albums")
 public class Album {
 
-	@Id 
-	@GeneratedValue (strategy = GenerationType.TABLE)
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	@Column(name = "album_id")
 	private int albumId;
 	@Column(name = "album_title")
 	private String albumTitle;
 	@ManyToOne
 	private User albumAuthor;
-	@OneToMany (fetch = FetchType.EAGER)
+	@OneToMany(fetch = FetchType.EAGER)
 	private List<Sound> albumTracks;
 	@Column(name = "album_cover")
-	private byte[] albumCover; //(photo)
+	private String fileName; // (photo)
 	@ManyToMany
 	@Column(name = "album_genres") // mai tva beshe one-to-many pregledai
 	private List<Genre> albumGenres;
-	
-	
-	
+
 	public int getAlbumId() {
 		return albumId;
 	}
+
 	public void setAlbumId(int albumId) {
 		this.albumId = albumId;
 	}
+
 	public String getAlbumTitle() {
 		return albumTitle;
 	}
-	public void setAlbumTitle(String albumTitle) {
+
+	public Album setAlbumTitle(String albumTitle) {
 		this.albumTitle = albumTitle;
+		return this;
 	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public Album setFileName(String fileName) {
+		this.fileName = fileName;
+		return this;
+	}
+
 	public User getAlbumAuthor() {
 		return albumAuthor;
 	}
-	public void setAlbumAuthor(User albumAuthor) {
+
+	public Album setAlbumAuthor(User albumAuthor) {
 		this.albumAuthor = albumAuthor;
+		return this;
 	}
+
 	public List<Sound> getAlbumTracks() {
 		return albumTracks;
 	}
+
 	public void setAlbumTracks(List<Sound> albumTracks) {
 		this.albumTracks = albumTracks;
 	}
-	public byte[] getAlbumCover() {
-		return albumCover;
-	}
-	public void setAlbumCover(byte[] albumCover) {
-		this.albumCover = albumCover;
-	}
+
 	public List<Genre> getAlbumGenres() {
 		return albumGenres;
 	}
-	public void setAlbumGenres(List<Genre> albumGenres) {
+
+	public Album setAlbumGenres(List<Genre> albumGenres) {
 		this.albumGenres = albumGenres;
+		return this;
 	}
+
 	public void removeSound(Sound sound) {
 		albumTracks.remove(sound);
-		
 	}
-	
-	
-	
+
+	public void addGenre(Genre genre) {
+		albumGenres.add(genre);
+	}
+
 }
