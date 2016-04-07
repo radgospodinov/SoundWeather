dfda3ab108850e1b1ced0322c68c5bd5628408ad
radgospodinov Radoslav Gospodinov <rad.gospodinov@gmail.com>
2016-04-07 23:51:22 +0300

    createAlbum method created - add album to user and put album into db

--- SoundWeather/src/main/java/controller/SoundController.java
+++ SoundWeather/src/main/java/controller/SoundController.java
@@ -1,17 +1,31 @@
 package controller;
 
+import java.awt.event.FocusAdapter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.neo4j.cypher.internal.compiler.v2_1.ast.rewriters.flattenBooleanOperators;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.google.gson.JsonObject;
 
 import model.Album;
+import model.Genre;
 import model.HibernateUtil;
 import model.Sound;
 import model.User;
@@ -19,6 +33,9 @@ import scala.languageFeature.reflectiveCalls;
 
 @Controller
 public class SoundController {
+
+	@Autowired
+	ServletContext context;
 
 	@RequestMapping(value = "/deleteSound", method = RequestMethod.POST)
 	public @ResponseBody String deleteSound(HttpServletRequest request) {
@@ -36,9 +53,9 @@ public class SoundController {
 					break;
 				}
 			}
-			for(Album album : u.getAlbums()){
-				for(Sound sound : album.getAlbumTracks()) {
-					if(sound.getSoundId()== id) {
+			for (Album album : u.getAlbums()) {
+				for (Sound sound : album.getAlbumTracks()) {
+					if (sound.getSoundId() == id) {
 						album.removeSound(sound);
 						break;
 					}
@@ -64,4 +81,61 @@ public class SoundController {
 		rv.addProperty("id", "#s" + id);
 		return rv.toString();
 	}
+
+	@RequestMapping(value = "/createAlbum", method = RequestMethod.POST)
+	public @ResponseBody String createAlbum(MultipartHttpServletRequest request) throws IOException {
+		JsonObject rv = new JsonObject();
+		// getting Album params
+		User user = (User) request.getSession().getAttribute("loggedUser");
+		String fileName = user.getUsername() + "_"
+				+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+		String title = request.getParameter("albumTitle");
+		String genresTmp = request.getParameter("albumGenres");
+		String[] genres = genresTmp.split(",");
+
+		MultipartFile albumCover = request.getFile("albumCover");
+		byte[] coverPhoto = null;
+		try {
+			coverPhoto = albumCover.getBytes();
+		} catch (IOException e) {
+			e.printStackTrace();
+			rv.addProperty("status", "bad");
+			return rv.toString();
+		}
+		// saving album to DB
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			User author = (User) session.get(User.class, user.getUsername());
+			Album album = new Album().setAlbumAuthor(author).setAlbumTitle(title).setFileName(fileName);
+			for (String string : genres) {
+				Genre genre = (Genre) session.get(Genre.class, Integer.parseInt(string));
+				album.addGenre(genre);
+			}
+			session.save(album);
+			author.addAlbum(album);
+			session.update(author);
+			tx.commit();
+			rv.addProperty("status", "ok");
+		} catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+			rv.addProperty("status", "bad");
+			return rv.toString();
+		} finally {
+			session.close();
+		}
+		// TODO save cover to server's file system
+		FileOutputStream fos = null;
+		File coverFile = new File(context.getRealPath("/static/covers/" + fileName + ".jpg"));
+		coverFile.createNewFile();
+		fos = new FileOutputStream(coverFile);
+		fos.write(coverPhoto);
+		fos.close();
+		return rv.toString();
+	}
+
 }
